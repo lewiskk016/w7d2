b176fa753fc8ed49b0e028295945d5909da6ef58
lewiskk016 <lewiskk016@gmail.com>
2023-04-18 16:05:02 -0400

    almost done with phase 0

--- Kevin_Lewis_practice-for-ch-rails-music-app-long-practice-main/MusicApp/app/controllers/Sessions_controller.rb
+++ Kevin_Lewis_practice-for-ch-rails-music-app-long-practice-main/MusicApp/app/controllers/Sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController
-    
+    def create
+        user = User.find_by_credentials(parms[:user][:username], params[:user][:password])
+        if user.nil?
+            flash.now[:errors] = "Invalid email or password"
+            render :new
+        else
+            login!(user)
+            redirect_to user_path(user)
+        end
+    end
+
+    def destroy
+        session.delete(:session_token)
+        redirect_to root_path
+      end
+
 end
--- Kevin_Lewis_practice-for-ch-rails-music-app-long-practice-main/MusicApp/app/controllers/Users_controller.rb
+++ Kevin_Lewis_practice-for-ch-rails-music-app-long-practice-main/MusicApp/app/controllers/Users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-    
+    before_action :require_logged_out
+
+    def new
+        @user = User.new
+        render :new
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
