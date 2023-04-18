91cc2f0195df753184bea7f6880c4cac9cdcb2a0
lewiskk016 <lewiskk016@gmail.com>
2023-04-18 14:10:58 -0400

    stil phase 0

---
+++ Kevin_Lewis_practice-for-ch-rails-music-app-long-practice-main/MusicApp/app/models/User.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def generate_unique_session_token
+        session_token = SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64(16)
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
